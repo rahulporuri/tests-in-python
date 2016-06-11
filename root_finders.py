257cb9d54eb1346279b7b327d30a82108f598a4f
Poruri Sai Rahul <rporuri@enthought.com>
2016-06-11 15:53:34 +0530

    ADD/FIX : add newton_rhapson, change bisection

--- root_finders.py
+++ root_finders.py
@@ -3,27 +3,26 @@ class Bisection():
 
     """
 
-    def __init__(self, function_array, initial_guess):
-        self.function_array = function_array
+    def __init__(self, function, initial_guess):
+        self.function = function
         self.a, self.b = initial_guess
 
     def _find_root(self):
-        function = lambda x : sum([coeff*(x**power) for coeff, power in self.function_array[:-1]])+self.function_array[-1]
-        func_value = function(self.a)*function(self.b)
+        func_value = self.function(self.a)*self.function(self.b)
         if abs(func_value) + func_value != 0:
             self.a = raw_input()
             self.b = raw_input()
         else:
             estimate_root = (self.a + self.b)/2.
-            while function(estimate_root) != 0:
-                func_value = function(self.a)*function(estimate_root)
+            while self.function(estimate_root) != 0:
+                func_value = self.function(self.a)*self.function(estimate_root)
                 if abs(func_value) + func_value != 0:
                     self.a = estimate_root
                     estimate_root = (self.a + self.b)/2.
                 else:
                     self.b = estimate_root
                     estimate_root = (self.a + self.b)/2.
-            
+
         self.root = estimate_root
 
 
@@ -55,15 +54,42 @@ class Secant():
         self.root = estimate_root
 
 
+class NewtonRhapson():
+    """
+
+    """
+
+    def __init__(self, function, Dfunction, initial_guess):
+        self.function = function
+        self.Dfunction = Dfunction
+        self.a = initial_guess
+
+    def _find_root(self):
+        estimate_root = self.a
+        func_value = self.function(estimate_root)
+        while func_value != 0:
+            estimate_root = estimate_root - self.function(estimate_root)/self.Dfunction(estimate_root)
+            func_value = self.function(estimate_root)
+
+        self.root = estimate_root
+
 
 if __name__ == "__main__":
     print "bisection"
-    bisection = Bisection([(1,3), (1,2), (1,1),-2], [-1,1])
+    function = lambda x: x**3+x**2+x-2
+    bisection = Bisection(function, [-1, 1])
     bisection._find_root()
     print bisection.root
 
     print "secant"
-    function = lambda x : x**2-4
-    secant = Secant(function, [-5,0])
+    function = lambda x: x**2-4
+    secant = Secant(function, [-5, 0])
     secant._find_root()
     print secant.root
+
+    print "newton-rhapson"
+    function = lambda x: x**2-4
+    dfunction = lambda x: 2*x
+    newton_rhapson = NewtonRhapson(function, dfunction, -4)
+    newton_rhapson._find_root()
+    print newton_rhapson.root
--- test_root_finders.py
+++ test_root_finders.py
@@ -1,6 +1,7 @@
 import unittest
 
-from root_finders import Bisection, Secant
+from root_finders import Bisection, Secant, NewtonRhapson
+
 
 class testRootFinders(unittest.TestCase):
     """
@@ -8,51 +9,62 @@ class testRootFinders(unittest.TestCase):
     """
 
     def test_bisection_linear_function(self):
-        function_array = [(1,1),-1]
-        initial_guess = [-5,5]
+        function = lambda x: x-1
+        initial_guess = [-5, 5]
         root = 1
 
-        bisection = Bisection(function_array, initial_guess)
+        bisection = Bisection(function, initial_guess)
         bisection._find_root()
 
         self.assertEqual(root, bisection.root)
-    
+
     def test_bisection_quadratic_function_1(self):
-        function_array = [(1,2),-4]
-        initial_guess = [-5,0]
+        function = lambda x: x**2-4
+        initial_guess = [-5, 0]
         root = -2
 
-        bisection = Bisection(function_array, initial_guess)
+        bisection = Bisection(function, initial_guess)
         bisection._find_root()
 
         self.assertEqual(root, bisection.root)
-        
+
     def test_bisection_quadratic_function_2(self):
-        function_array = [(1,2),-4]
-        initial_guess = [0,5]
+        function = lambda x: x**2-4
+        initial_guess = [0, 5]
         root = 2
 
-        bisection = Bisection(function_array, initial_guess)
+        bisection = Bisection(function, initial_guess)
         bisection._find_root()
 
         self.assertEqual(root, bisection.root)
-    
-    def test_newton_rhapson_linear_function(self):
-        function = lambda x : x-1 
-        initial_guess = [-5,5]
+
+    def test_secant_linear_function(self):
+        function = lambda x: x-1
+        initial_guess = [-5, 5]
         root = 1
 
         secant = Secant(function, initial_guess)
         secant._find_root()
 
         self.assertEqual(root, secant.root)
-    
+
     def test_secant_quadratic_function_1(self):
         function = lambda x: x**2-4
-        initial_guess = [-5,0]
+        initial_guess = [-5, 0]
         root = -2
 
         secant = Secant(function, initial_guess)
         secant._find_root()
 
         self.assertEqual(root, secant.root)
+
+    def test_newton_rhapson_quadratic_function_1(self):
+        function = lambda x: x**2-4
+        Dfunction = lambda x: 2*x
+        initial_guess = -4
+        root = -2
+
+        newton_rhapson = NewtonRhapson(function, Dfunction, initial_guess)
+        newton_rhapson._find_root()
+
+        self.assertEqual(root, newton_rhapson.root)
