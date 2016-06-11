45c58c24b697ace019fb949b8c265bc025906589
Poruri Sai Rahul <rporuri@enthought.com>
2016-06-11 14:36:50 +0530

    ADD : newton-rhapson methods and tests

--- root_finders.py
+++ root_finders.py
@@ -27,6 +27,42 @@ class Bisection():
         self.root = estimate_root
 
 
+class NewtonRhapson():
+    """
+
+    """
+
+    def __init__(self, function, initial_guess):
+        self.function = function
+        self.a, self.b = initial_guess
+
+    def _find_root(self):
+        func_value = self.function(self.a)*self.function(self.b)
+        if abs(func_value) + func_value != 0:
+            self.a = raw_input('a=')
+            self.b = raw_input('b=')
+        else:
+            estimate_root = (self.a*self.function(self.b) - self.b*self.function(self.a))/(self.function(self.b) - self.function(self.a))
+            while self.function(estimate_root) != 0:
+                func_value = self.function(self.a)*self.function(estimate_root)
+                if abs(func_value) + func_value != 0:
+                    self.a = estimate_root
+                    estimate_root = (self.a*self.function(self.b) - self.b*self.function(self.a))/(self.function(self.b) - self.function(self.a))
+                else:
+                    self.b = estimate_root
+                    estimate_root = (self.a*self.function(self.b) - self.b*self.function(self.a))/(self.function(self.b) - self.function(self.a))
+
+        self.root = estimate_root
+
+
 if __name__ == "__main__":
+    print "bisection"
     bisection = Bisection([(1,3), (1,2), (1,1),-2], [-1,1])
     bisection._find_root()
+    print bisection.root
+
+    print "newton rhapson"
+    function = lambda x : x**2-4
+    newton_rhapson = NewtonRhapson(function, [-5,0])
+    newton_rhapson._find_root()
+    print newton_rhapson.root
--- test_root_finders.py
+++ test_root_finders.py
@@ -1,6 +1,6 @@
 import unittest
 
-from root_finders import Bisection
+from root_finders import Bisection, NewtonRhapson
 
 class testRootFinders(unittest.TestCase):
     """
@@ -17,7 +17,7 @@ class testRootFinders(unittest.TestCase):
 
         self.assertEqual(root, bisection.root)
     
-    def test_bisection_quadratic_function(self):
+    def test_bisection_quadratic_function_1(self):
         function_array = [(1,2),-4]
         initial_guess = [-5,0]
         root = -2
@@ -27,6 +27,7 @@ class testRootFinders(unittest.TestCase):
 
         self.assertEqual(root, bisection.root)
         
+    def test_bisection_quadratic_function_2(self):
         function_array = [(1,2),-4]
         initial_guess = [0,5]
         root = 2
@@ -35,3 +36,23 @@ class testRootFinders(unittest.TestCase):
         bisection._find_root()
 
         self.assertEqual(root, bisection.root)
+    
+    def test_newton_rhapson_linear_function(self):
+        function = lambda x : x-1 
+        initial_guess = [-5,5]
+        root = 1
+
+        newton_rhapson = NewtonRhapson(function, initial_guess)
+        newton_rhapson._find_root()
+
+        self.assertEqual(root, newton_rhapson.root)
+    
+    def test_newton_rhapson_quadratic_function_1(self):
+        function = lambda x: x**2-4
+        initial_guess = [-5,0]
+        root = -2
+
+        newton_rhapson = NewtonRhapson(function, initial_guess)
+        newton_rhapson._find_root()
+
+        self.assertEqual(root, newton_rhapson.root)
